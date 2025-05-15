b99c1da685917c3f83459ddd0de209622812503d
Naufal Akmal Rosaidi <112294497+Naakro@users.noreply.github.com>
2025-05-15 20:42:49 +0700

    feature/fe-contact-Naufal (#28)
    * feat: menambahkan bagian Kontak Kami di halaman contact
    
    * feat: membuat halaman responsive di halaman contact

--- fe/src/Contact/main.js
+++ fe/src/Contact/main.js
@@ -1,6 +1,10 @@
 import '../global.css'
 import './style.css'
 import logoImage from '/logo.svg'
+import iconPhone from '/telephone.svg'
+import iconEmail from '/email.svg'
+import iconInstagram from '/instagram.svg'
+import iconLocation from '/maps.svg'
 import { createNavbarHTML,initNavbarFunctionality} from '../Component/Navbar/navbar.js'
 
 
@@ -10,10 +14,58 @@ document.querySelector('#contact').innerHTML = `
     schoolName: 'TK & KB SITI HAJAR',
     activePage: 'contact'
   })}
-  <div>
-    <h1>Contact</h1>
-  </div>
-`
+
+  <section class="contact-section">
+    <h1>KONTAK KAMI</h1>
+    <div class="contact-content">
+      <div class="contact-info">
+        <div class="contact-item">
+          <img src="${iconPhone}" alt="Phone Icon" />
+          <div class="contact-text">
+            <strong>Contact Person :</strong>
+            <span>+628813325407 (Bunda Niniek)</span>
+          </div>
+        </div>
+
+        <div class="contact-item">
+          <img src="${iconEmail}" alt="Email Icon" />
+          <div class="contact-text">
+            <strong>Email :</strong>
+            <span>sitihajarkarlos@gmail.com</span>
+          </div>
+        </div>
+
+        <div class="contact-item">
+          <img src="${iconInstagram}" alt="Instagram Icon" />
+          <div class="contact-text">
+            <strong>Instagram :</strong>
+            <span>bundasitihajarkarangploso</span>
+          </div>
+        </div>
+
+        <div class="contact-item">
+          <img src="${iconLocation}" alt="Location Icon" />
+          <div class="contact-text">
+            <strong>Alamat :</strong>
+            <span>Jl. Ampel Pratama V Blok E 2 No. 9, Perum Pesanggrahan Pratama, Karangploso, Kab. Malang</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="contact-map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.9565489799265!2d112.59119647536684!3d-7.8996085921233625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7881bf0f69033f%3A0x7ce831bb01b01cb2!2sPAUD%20TERPADU%20SITI%20HAJAR!5e0!3m2!1sid!2sid!4v1747291106600!5m2!1sid!2sid"
+          width="600"
+          height="450"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+      </div>
+    </div>
+  </section>
+`;
 
 initNavbarFunctionality();
 
