fae8745b1018b270e3d5dde43ff1ba1f69a84a21
Kentangz <adhitsholawat12@gmail.com>
2025-06-13 11:22:27 +0700

    feat(auth): enhance login UI with improved styling and structure

--- fe/admin-tkkb-sitihajar-karlos/Auth/main.js
+++ fe/admin-tkkb-sitihajar-karlos/Auth/main.js
@@ -1,16 +1,21 @@
 import "../global.css";
+import "./style.css";
 import { handleLogin } from "./Api/Login/handleLogin";
 
 const Login = document.querySelector("#login");
 
 if (Login) {
 	Login.innerHTML = `
-    <form id="loginForm">
-      <input type="email" id="email" placeholder="Email" required />
-      <input type="password" id="password" placeholder="Password" required />
-      <button type="submit">Login</button>
-    </form>
-    <div id="error" style="color:red;"></div>
+    <h1>Welcome, Log into you account</h1>
+    <div class="login-container">
+      <p>It is our great pleasure to have you on board!</p>
+      <form id="loginForm">
+        <input type="text" id="email" placeholder="Enter Email" required />
+        <input type="password" id="password" placeholder="Enter Password" required />
+        <button type="submit">Login</button>
+      </form>
+      <div id="error"></div>
+    </div>
   `;
 
 	handleLogin();
